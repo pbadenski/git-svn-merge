55c04c6ae63680f9e611d0f59ab71da9ee94c8ba
Pawel Badenski <Pawel.Badenski@gmail.com>
2011-05-18 23:05:25 +0200

    Support merging of ranges of commits via two branches.

---
+++ tests/test-merge-range-of-commits-via-two-branches.sh
@@ -0,0 +1,39 @@
+#!/bin/zsh
+BASE_LOG_DIR=`pwd`
+source helper.sh
+
+{
+setup_svn_repository
+
+git svn clone -T trunk -b branches file:///$SVN_REPO_DIR/svn-project git-svn-project
+
+cd git-svn-project
+	git co trunk
+		touch beta
+		git add beta
+		git commit -m "Beta"
+		git svn dcommit
+		touch gamma
+		git add gamma
+		git commit -m "Gamma"
+		git svn dcommit
+		
+	git co feature
+		git svn merge `git rev-list --reverse trunk -2`
+		git log -1
+		git svn dcommit
+		git svn propget svn:mergeinfo
+} > $BASE_LOG_DIR/$0.log 2>&1
+		assert_equals `git svn propget svn:mergeinfo` "/svn-project/trunk:8,9"
+
+{
+	git co feature_2
+		git svn cherry-pick -m 1 `git rev-list --reverse feature -1`
+		git log -1
+		git svn dcommit
+		git svn propget svn:mergeinfo
+} > $BASE_LOG_DIR/$0.log 2>&1
+		assert_contains "`git svn propget svn:mergeinfo`" "/svn-project/trunk:8-9"
+		assert_contains "`git svn propget svn:mergeinfo`" "/svn-project/branches/feature:4-5,10"
+cd ..
+
